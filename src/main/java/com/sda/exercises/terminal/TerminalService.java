6f0250cdf482cd33be523e52fff43bcfec5ecd20
Mateusz Wierzbicki <matvwierzbicki@gmail.com>
2019-06-22 14:19:30 +0200

    Dodanie klasy TerminalService

---
+++ src/main/java/com/sda/exercises/terminal/TerminalService.java
@@ -0,0 +1,4 @@
+package com.sda.exercises.terminal;
+
+public class TerminalService {
+}
---
+++ src/test/java/com/sda/exercises/test/TerminalServiceTest.java
@@ -0,0 +1,10 @@
+package com.sda.exercises.test;
+
+import com.sda.exercises.terminal.TerminalService;
+
+public class TerminalServiceTest {
+    private TerminalService terminalService;
+    private int number;
+
+
+}
